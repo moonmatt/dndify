ca1dc887ad0ce503325cd64ef7628e3d69ef2049
MatteoGalavotti <pisucca11@gmail.com>
2019-10-03 21:08:06 +0200

    fixed

--- includes/dbh.php
+++ includes/dbh.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 // Code for the Database Table
 
 // CREATE TABLE transactions (
-//   id int(11) NOT NULL
+//   id int(11) not null PRIMARY KEY AUTO_INCREMENT,
 //   date date NOT NULL,
 //   value decimal(10,2) NOT NULL,
 //   type int(1) NOT NULL,
